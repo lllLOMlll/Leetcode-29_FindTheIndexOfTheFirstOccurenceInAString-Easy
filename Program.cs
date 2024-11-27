dcdc49652180c9e056f8e6a104bfcff89b86c036
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-11-27 06:54:11 -0500

    All tests passed

--- Program.cs
+++ Program.cs
@@ -5,17 +5,16 @@
         if (!haystack.Contains(needle)) return -1;
 
         int indexCount = 0;
-        foreach (char letter in haystack)
-        {
-            if (letter == needle[0])
-            {
-                return indexCount;
-            }
-            else
-            {
-                indexCount++;
-            }
+        string haystackStringByString = "";
 
+        for (int i = 0; i < haystack.Length; i++)
+        {
+           haystackStringByString += haystack[i];
+           if (haystackStringByString.Contains(needle))
+           {
+            return (indexCount+1-needle.Length);
+           }
+           indexCount ++;
         }
         return indexCount;
     }
@@ -23,10 +22,14 @@
     static void Main(string[] args)
     {
         Solution s = new Solution();
-
-        // Console.WriteLine("0 -> " + s.StrStr("sadbutsad", "sad"));
-        // Console.WriteLine("-1 -> " + s.StrStr("leetcode", "leeto"));
-        // Console.WriteLine("4 -> " + s.StrStr("leetcode", "code"));
+        string test = "test";
+        string test2 = "test" + "1";
+        // Console.WriteLine(test[2]);
+        // Console.WriteLine("test2 ->" + test2);
+        Console.WriteLine("0 -> " + s.StrStr("sadbutsad", "sad"));
+        Console.WriteLine("-1 -> " + s.StrStr("leetcode", "leeto"));
+        Console.WriteLine("4 -> " + s.StrStr("leetcode", "code"));
+        Console.WriteLine("mississippi");
         Console.WriteLine("4 -> " + s.StrStr("mississippi", "issip"));
     }
 }
