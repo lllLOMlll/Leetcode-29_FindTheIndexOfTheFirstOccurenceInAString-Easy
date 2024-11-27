51601096ab00b2e518f4c08647c8702599b974e2
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-11-26 19:23:02 -0500

    64/83 testcases passed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        if (!haystack.Contains(needle)) return -1;
+
+        int indexCount = 0;
+        foreach (char letter in haystack)
+        {
+            if (letter == needle[0])
+            {
+                return indexCount;
+            }
+            else
+            {
+                indexCount++;
+            }
+
+        }
+        return indexCount;
+    }
+
+    static void Main(string[] args)
+    {
+        Solution s = new Solution();
+
+        // Console.WriteLine("0 -> " + s.StrStr("sadbutsad", "sad"));
+        // Console.WriteLine("-1 -> " + s.StrStr("leetcode", "leeto"));
+        // Console.WriteLine("4 -> " + s.StrStr("leetcode", "code"));
+        Console.WriteLine("4 -> " + s.StrStr("mississippi", "issip"));
+    }
+}
